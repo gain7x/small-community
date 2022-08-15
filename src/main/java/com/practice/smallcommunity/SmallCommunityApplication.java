a764bb692fa4b55a0dbfcac759b11337c71db2cf
CIJ <gain7x@gmail.com>
2022-08-15 16:10:20 +0900

    프로젝트 생성 및 기초 설정

---
+++ src/main/java/com/practice/smallcommunity/SmallCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.practice.smallcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/smallcommunity/SmallCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.smallcommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
