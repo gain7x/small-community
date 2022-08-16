71fcb8cbc94197214c69d5f11258f03f52f31b14
CIJ <gain7x@gmail.com>
2022-08-17 00:52:31 +0900

    feat(post): 카테고리 엔티티 추가

---
+++ src/main/java/com/practice/smallcommunity/domain/post/Category.java
@@ -0,0 +1,42 @@
+package com.practice.smallcommunity.domain.post;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @JoinColumn(name = "parent_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Category parent;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private boolean enabled;
+
+    @Builder
+    private Category(Long id, Category parent, String name, boolean enabled) {
+        this.id = id;
+        this.parent = parent;
+        this.name = name;
+        this.enabled = enabled;
+    }
+}
