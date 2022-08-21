2c063ca3737fdcd90bb237f427edc495801e9812
CIJ <gain7x@gmail.com>
2022-08-21 17:00:58 +0900

    feat(jwt): 토큰에 권한 정보 추가

--- src/main/java/com/practice/smallcommunity/security/JwtTokenService.java
+++ src/main/java/com/practice/smallcommunity/security/JwtTokenService.java
@@ -1,6 +1,9 @@
 package com.practice.smallcommunity.security;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.practice.smallcommunity.domain.member.Member;
+import com.practice.smallcommunity.domain.member.RoleType;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
@@ -8,23 +11,34 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import java.nio.charset.StandardCharsets;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 @RequiredArgsConstructor
 public class JwtTokenService {
 
+    private static final String ISSUER = "S-Community";
+    private static final String ROLE_CLAIM = "ROLE";
+
     @Value("${jwt.secret-key}")
     private String secretKey = "dummy";
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     public String createToken(Member member) {
+        String roleClaim = getRoleClaim(member);
 
         return Jwts.builder()
             .signWith(SignatureAlgorithm.HS512, secretKey.getBytes(StandardCharsets.UTF_8))
-            .setIssuer("S-Community")
+            .setIssuer(ISSUER)
             .setIssuedAt(new Date())
             .setSubject(member.getUsername())
             .setExpiration(
@@ -32,6 +46,7 @@ public class JwtTokenService {
                     Instant.now()
                         .plus(1, ChronoUnit.DAYS))
             )
+            .claim(ROLE_CLAIM, roleClaim)
             .compact();
     }
 
@@ -40,15 +55,41 @@ public class JwtTokenService {
             Jws<Claims> claims = Jwts.parser()
                 .setSigningKey(secretKey.getBytes(StandardCharsets.UTF_8))
                 .parseClaimsJws(jwtToken);
+
             Claims body = claims.getBody();
+            List<GrantedAuthority> authorities = getAuthoritiesFromClaims(body);
 
             return new UsernamePasswordAuthenticationToken(
                 body.getSubject(),
                 null,
-                null
+                authorities
             );
         } catch (Exception e) {
             return null;
+        }
+    }
+
+    private String getRoleClaim(Member member) {
+        List<RoleType> roleTypes = member.getMemberRoles().stream()
+            .map(memberRole -> memberRole.getRole().getRoleType())
+            .collect(Collectors.toList());
+
+        try {
+            return objectMapper.writeValueAsString(roleTypes);
+        } catch (JsonProcessingException e) {
+            throw new IllegalArgumentException("회원 권한 토큰화에 실패했습니다.", e);
+        }
+    }
+
+    private List<GrantedAuthority> getAuthoritiesFromClaims(Claims body) {
+        try {
+            String roleClaim = body.get(ROLE_CLAIM, String.class);
+
+            return Arrays.stream(objectMapper.readValue(roleClaim, RoleType[].class))
+                .map(roleType -> new SimpleGrantedAuthority(roleType.name()))
+                .collect(Collectors.toList());
+        } catch (JsonProcessingException e) {
+            throw new IllegalArgumentException("회원 권한 정보 추출에 실패했습니다.", e);
         }
     }
 }
--- src/test/java/com/practice/smallcommunity/security/JwtTokenServiceTest.java
+++ src/test/java/com/practice/smallcommunity/security/JwtTokenServiceTest.java
@@ -1,8 +1,10 @@
 package com.practice.smallcommunity.security;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import com.practice.smallcommunity.domain.member.Member;
+import com.practice.smallcommunity.domain.member.Role;
+import com.practice.smallcommunity.domain.member.RoleType;
 import org.junit.jupiter.api.Test;
 import org.springframework.security.core.Authentication;
 
@@ -36,5 +38,41 @@ class JwtTokenServiceTest {
 
         //then
         assertThat(authentication).isNotNull();
+    }
+
+    @Test
+    void 토큰에_권한정보_추가() {
+        //given
+        Role role = Role.builder()
+            .roleType(RoleType.ROLE_USER)
+            .build();
+
+        member.addRole(role);
+
+        //when
+        String token = jwtTokenService.createToken(member);
+
+        //then
+        assertThat(token).isNotNull();
+    }
+
+    @Test
+    void 토큰에_권한정보가_있으면_인증객체에_존재한다() {
+        //given
+        Role role = Role.builder()
+            .roleType(RoleType.ROLE_USER)
+            .build();
+
+        member.addRole(role);
+
+        String token = jwtTokenService.createToken(member);
+
+        //when
+        Authentication authentication = jwtTokenService.getAuthentication(token);
+
+        //then
+        assertThat(authentication.getAuthorities().size()).isEqualTo(1);
+        assertThat(authentication.getAuthorities()).allMatch(
+            auth -> auth.getAuthority().equals(RoleType.ROLE_USER.name()));
     }
 }
