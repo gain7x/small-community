5e0b164c2add9cfe24cfbd67ccb093ff3bb7f692
CIJ <gain7x@gmail.com>
2022-08-25 16:59:06 +0900

    test: DisplayNameGeneration 기본값 설정
    DisplayNameGenerator.ReplaceUnderscores 기본 적용

--- src/test/java/com/practice/smallcommunity/domain/board/BoardRepositoryTest.java
+++ src/test/java/com/practice/smallcommunity/domain/board/BoardRepositoryTest.java
@@ -2,18 +2,13 @@ package com.practice.smallcommunity.domain.board;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.practice.smallcommunity.domain.board.Board;
-import com.practice.smallcommunity.domain.board.BoardRepository;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @DataJpaTest
 class BoardRepositoryTest {
 
