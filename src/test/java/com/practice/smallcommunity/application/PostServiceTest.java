a37561fe9fb40b4a16d6346cb9f9df23eca2a578
CIJ <gain7x@gmail.com>
2022-08-30 20:15:06 +0900

    test(post): 조회 테스트 추가

--- src/test/java/com/practice/smallcommunity/application/PostServiceTest.java
+++ src/test/java/com/practice/smallcommunity/application/PostServiceTest.java
@@ -1,10 +1,12 @@
 package com.practice.smallcommunity.application;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.when;
 
 import com.practice.smallcommunity.application.dto.PostDto;
+import com.practice.smallcommunity.application.exception.ValidationErrorException;
 import com.practice.smallcommunity.domain.category.Category;
 import com.practice.smallcommunity.domain.member.Member;
 import com.practice.smallcommunity.domain.post.Post;
@@ -28,6 +30,7 @@ class PostServiceTest {
 
     Category category = DomainGenerator.createCategory("dev", "개발");
     Member member = DomainGenerator.createMember("A");
+    Post dummyPost = DomainGenerator.createPost(category, member, "내용");
 
     @BeforeEach
     void setUp() {
@@ -49,12 +52,36 @@ class PostServiceTest {
     }
 
     @Test
-    void 게시글을_수정한다() {
+    void 게시글을_ID로_조회할_때_일치하는_ID가_없으면_예외를_던진다() {
         //given
-        Post post = DomainGenerator.createPost(category, member, "내용");
+        when(postRepository.findById(1L))
+            .thenReturn(Optional.empty());
+
+        //when
+        //then
+        assertThatThrownBy(() -> postService.findEnabledPost(1L))
+            .isInstanceOf(ValidationErrorException.class);
+    }
+
+    @Test
+    void 게시글을_ID로_조회했는데_삭제상태이면_예외를_던진다() {
+        //given
+        dummyPost.delete();
 
         when(postRepository.findById(1L))
-            .thenReturn(Optional.of(post));
+            .thenReturn(Optional.of(dummyPost));
+
+        //when
+        //then
+        assertThatThrownBy(() -> postService.findEnabledPost(1L))
+            .isInstanceOf(ValidationErrorException.class);
+    }
+
+    @Test
+    void 게시글을_수정한다() {
+        //given
+        when(postRepository.findById(1L))
+            .thenReturn(Optional.of(dummyPost));
 
         //when
         Post updatedPost = postService.update(1L,
@@ -68,15 +95,13 @@ class PostServiceTest {
     @Test
     void 게시글을_삭제한다() {
         //given
-        Post post = DomainGenerator.createPost(category, member, "내용");
-
         when(postRepository.findById(1L))
-            .thenReturn(Optional.of(post));
+            .thenReturn(Optional.of(dummyPost));
 
         //when
         postService.disable(1L);
 
         //then
-        assertThat(post.isEnable()).isFalse();
+        assertThat(dummyPost.isEnable()).isFalse();
     }
 }
