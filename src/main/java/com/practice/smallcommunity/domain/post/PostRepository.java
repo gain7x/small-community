ae13983d5024313966ba8cb09bb0413491a2c7f3
CIJ <gain7x@gmail.com>
2022-12-13 23:09:01 +0900

    refactor(jpa): @EntityGraph 대신 명시적으로 페치 조인 사용
    @EntityGraph 어노테이션 사용 시 LEFT OUTER JOIN이 실행됩니다.
    쿼리에 명시적으로 페치 조인 구문을 사용하여 쿼리 예측이 쉽도록 개선합니다.

--- src/main/java/com/practice/smallcommunity/domain/post/PostRepository.java
+++ src/main/java/com/practice/smallcommunity/domain/post/PostRepository.java
@@ -3,7 +3,6 @@ package com.practice.smallcommunity.domain.post;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,12 +12,11 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     Optional<Post> findByIdAndEnableIsTrue(Long id);
 
     /**
-     * 삭제 상태가 아닌 게시글을 조회하며 본문까지 페치조인으로 가져옵니다.
+     * 삭제 상태가 아닌 게시글을 조회합니다.
      * @param id 게시글 ID
      * @return 게시글
      */
-    @Query("select p from Post p where p.id = :id and p.enable is true")
-    @EntityGraph(attributePaths = {"mainText"})
+    @Query("select p from Post p join fetch p.mainText join fetch p.category where p.id = :id and p.enable is true")
     Optional<Post> findPostFetchJoin(@Param("id") Long id);
 
     /**
--- src/main/java/com/practice/smallcommunity/domain/reply/ReplyRepository.java
+++ src/main/java/com/practice/smallcommunity/domain/reply/ReplyRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -24,7 +23,7 @@ public interface ReplyRepository extends JpaRepository<Reply, Long> {
      * @param pageable 페이징 정보
      * @return 답글 목록
      */
-    @Query("select r from Reply r where r.writer.id = :writerId and r.enable is true")
-    @EntityGraph(attributePaths = "post")
+    @Query(value = "select r from Reply r join fetch r.post where r.writer.id = :writerId and r.enable is true",
+        countQuery = "select count(r) from Reply r where r.writer.id = :writerId and r.enable is true")
     Page<Reply> findByWriterFetchJoin(@Param("writerId") Long writerId, Pageable pageable);
 }
