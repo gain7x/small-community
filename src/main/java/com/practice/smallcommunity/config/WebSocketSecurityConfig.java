976768007e790986b8acc25d51e6358a88699e57
gain7x <gain7x@gmail.com>
2023-04-04 15:39:33 +0900

    fix(WebSocket): CONNECT, DISCONNECT 프레임 전송 허용, CQRS 비활성화
    프론트는 JWT의 액세스 토큰을 쿠키가 아닌 헤더로 전송합니다. 이에 따른 설정입니다.

--- src/main/java/com/practice/smallcommunity/config/WebSocketSecurityConfig.java
+++ src/main/java/com/practice/smallcommunity/config/WebSocketSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.practice.smallcommunity.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.SimpMessageType;
 import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
 import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
 
@@ -10,9 +11,15 @@ public class WebSocketSecurityConfig extends AbstractSecurityWebSocketMessageBro
     @Override
     protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
         messages
-                .simpSubscribeDestMatchers("/user/topic/inquiry").authenticated()
-                .simpSubscribeDestMatchers("/topic/**", "/queue/**").hasRole("ADMIN")
-                .simpDestMatchers("/app/**").authenticated()
-                .anyMessage().denyAll();
+            .simpSubscribeDestMatchers("/user/topic/inquiry").authenticated()
+            .simpSubscribeDestMatchers("/topic/**", "/queue/**").hasRole("ADMIN")
+            .simpTypeMatchers(SimpMessageType.CONNECT, SimpMessageType.DISCONNECT).permitAll()
+            .simpDestMatchers("/app/**").authenticated()
+            .anyMessage().denyAll();
+    }
+
+    @Override
+    protected boolean sameOriginDisabled() {
+        return true;
     }
 }
